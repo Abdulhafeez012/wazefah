f5f4b3ee00566fb88f2a3bc2f6ec5d2e83383297
Abdulhafeez012 <aboodhammed92@gmail.com>
2022-02-07 18:20:56 +0200

    working on logIn and SignUp feature

--- Sitech/urls.py
+++ Sitech/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('wazefah.urls',namespace='wazefah'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('',include('wazefah.urls',namespace='wazefah')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEIDA_URL, document_root=settings.MEDIA_ROOT) 
