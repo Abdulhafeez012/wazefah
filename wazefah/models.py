6d6e79d0ac175e2837b226f339f6d203cc23fb69
hudajundi <huda.aljundi97@gmail.com>
2022-02-09 09:26:19 +0200

    add a job model

--- wazefah/models.py
+++ wazefah/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+# jobs table
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    company_name = models.TextField()
+    category = models.CharField(max_length=255)
