ae28c0c0a55c546cb00d4957f8a3eb4d1c05329e
Abdulhafeez Hamed Zalloom <56917669+Abdulhafeez012@users.noreply.github.com>
2022-02-09 13:41:26 +0200

    update admin.py
    cleaned the code

--- wazefah/admin.py
+++ wazefah/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-
+from .models import UserInformation
 from .models import Job
 
-
-# Register your models here.
 admin.site.register(Job)
-
-from .models import UserInformation
-# Register your models here.
 admin.site.register(UserInformation)
 
