82ab1edc682efae7a83d99d51af4624d84d89a36
Abdulhafeez Hamed Zalloom <56917669+Abdulhafeez012@users.noreply.github.com>
2022-02-22 12:45:49 +0200

    Update admin.py
    add the Experience model on admin register

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import UserInformation, Job,AppliedJob
+from .models import (
+  UserInformation, 
+  Job,
+  AppliedJob,
+  Experience,
+)
 
 admin.site.register(UserInformation)
 admin.site.register(Job)
 admin.site.register(AppliedJob)
+admin.site.register(Experience)
